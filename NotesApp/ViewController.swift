154f9b52c525e8dfdd04fe8676977c81d66a5b26
Andrew Sorokin <45689723+Andrewsorokin13@users.noreply.github.com>
2022-07-27 20:28:43 +0300

    Initial Commit

---
+++ NotesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotesApp
+//
+//  Created by Андрей Сорокин on 27/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
